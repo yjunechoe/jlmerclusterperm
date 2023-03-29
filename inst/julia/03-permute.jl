2e4af2c7ed1d9b4595c4c8b8f1434bea90b83e85
June Choe <yjchoe@sas.upenn.edu>
2023-03-29 09:29:36 -0400

    split files

---
+++ inst/julia/03-permute.jl
@@ -0,0 +1,28 @@
+function guess_and_shuffle_as!(df, predictor_cols, participant_col, trial_col)
+  shuffle_type = guess_shuffle_as(df, predictor_cols, participant_col, trial_col)
+  shuffle_as!(df, predictor_cols, participant_col, trial_col, shuffle_type)
+end
+
+function guess_shuffle_as(df, predictor_cols, participant_col, trial_col)
+  if (ismissing(trial_col))
+    return "between_participant"
+  else
+    subj_pred_pair = unique(df[!,vcat(participant_col, predictor_cols)])
+    between_participant = length(unique(df[!, participant_col])) == nrow(subj_pred_pair)
+    return return between_participant ? "between_participant" : "within_participant"
+  end
+end
+
+function shuffle_as!(df, shuffle_type, predictor_cols, participant_col, trial_col)
+  if shuffle_type == "between_participant"
+    subj_pred_pair = unique(df[!, vcat(participant_col, predictor_cols)])
+    shuffle!(rng, subj_pred_pair[!, participant_col])
+    select!(df, Not(predictor_cols))
+    leftjoin!(df, subj_pred_pair, on = participant_col)
+  elseif shuffle_type == "within_participant"
+    trial_pred_pair = unique(df[!, vcat(participant_col, trial_col, predictor_cols)])
+    combine(groupby(trial_pred_pair, participant_col), sdf -> shuffle!(rng, sdf[!, trial_col]))
+    select!(df, Not(predictor_cols))
+    leftjoin!(df, trial_pred_pair, on = [participant_col, trial_col])
+  end
+end
