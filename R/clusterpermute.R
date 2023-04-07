3b0b1dfd2158db9a11179eda1961cca6ca14c822
June Choe <yjchoe@sas.upenn.edu>
2023-04-07 15:23:18 -0400

    progress steps

--- R/clusterpermute.R
+++ R/clusterpermute.R
@@ -14,9 +14,12 @@ clusterpermute <- function(jlmer_spec,
                            binned, top_n,
                            add1,
                            ...) {
-  empirical_statistics <- compute_timewise_statistics(jlmer_spec, family, statistic, ...)
+  cli::cli_progress_step("Detecting empirical clusters and calculating cluster-mass statistics.")
+  empirical_statistics <- suppressMessages(compute_timewise_statistics(jlmer_spec, family, statistic, ...))
   empirical_clusters <- extract_empirical_clusters(empirical_statistics, threshold, binned, top_n)
+  cli::cli_progress_step("Sampling cluster-mass statistics from a bootstrapped null distribution.")
   null_statistics <- permute_timewise_statistics(jlmer_spec, family, statistic, nsim, predictors, ...)
   null_clusters <- extract_null_cluster_dists(null_statistics, threshold, binned)
+  cli::cli_progress_step("Calculating the probability of the observed cluster-mass statistics.")
   calculate_clusters_pvalues(empirical_clusters, null_clusters, add1)
 }
