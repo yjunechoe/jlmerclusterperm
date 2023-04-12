ebc85c0dbf70fb8a83a1eca6e09ec3197ffb3f80
June Choe <yjchoe@sas.upenn.edu>
2023-04-12 16:20:53 -0400

    default progress to false

--- R/jlmer.R
+++ R/jlmer.R
@@ -1,6 +1,6 @@
 #' Fit a Julia mixed model using lme4 syntax
 #'
-#' @param jlmer_spec_opts Other options passed to `make_jlmer_spec()`
+#' @param jlmer_spec_opts List of options passed to `make_jlmer_spec()`
 #' @inheritParams jlmer
 #' @inheritParams make_jlmer_spec
 #'
@@ -8,7 +8,7 @@
 #'
 #' @return A `jlmer_mod` object.
 #' @export
-to_jlmer <- function(formula, data, family = c("gaussian", "binomial"), jlmer_spec_opts = list(), ...) {
+to_jlmer <- function(formula, data, family = c("gaussian", "binomial"), jlmer_spec_opts = list(), ..., progress = FALSE) {
 
   jlmer_spec <- do.call(make_jlmer_spec, utils::modifyList(jlmer_spec_opts, list(formula = formula, data = data)))
   jlmer(jlmer_spec, family, ...)
@@ -20,17 +20,18 @@ to_jlmer <- function(formula, data, family = c("gaussian", "binomial"), jlmer_sp
 #' @param jlmer_spec Data prepped for jlmer from `make_jlmer_spec()`
 #' @param family A GLM family. Currently supports "gaussian" and "binomial".
 #' @param ... Optional arguments passed to Julia for model fitting.
+#' @param progress If `TRUE`, prints the timing of iterations.
 #'
 #' @seealso [make_jlmer_spec()]
 #'
 #' @return A `jlmer_mod` object.
 #' @export
-jlmer <- function(jlmer_spec, family = c("gaussian", "binomial"), ...) {
+jlmer <- function(jlmer_spec, family = c("gaussian", "binomial"), ..., progress = FALSE) {
 
   family <- match.arg(family)
   args <- prep_for_jlmer(jlmer_spec$formula$jl, jlmer_spec$data, time = NULL, family, ...)[-3]
 
-  mod <- do.call(.jlmerclusterperm$jlmer, c(args, jlmer_spec$meta$is_mem, ...))
+  mod <- do.call(.jlmerclusterperm$jlmer, c(args, jlmer_spec$meta$is_mem, progress = progress, ...))
   structure(mod, class = c("jlmer_mod", class(mod)))
 
 }
