68dde90210e1e42d0199331084daecbe39ccb48c
June Choe <yjchoe@sas.upenn.edu>
2023-04-13 21:58:35 -0400

    progress bar for walk threshold

--- R/threshold_search.R
+++ R/threshold_search.R
@@ -16,19 +16,12 @@ walk_threshold_steps <- function(empirical_statistics, null_statistics, threshol
     out <- tidy(calculate_clusters_pvalues(empirical, null, add1 = TRUE))
     out[!is.na(out$pvalue), ]
   }
-  res <- vector("list", length(threshold_steps))
-  i <- 1
-  cli::cli_progress_step("Testing {.arg threshold = {.val {threshold_steps[i]}}} ({i}/{length(threshold_steps)})")
-  for (i in seq_along(threshold_steps)) {
-    cli::cli_progress_update()
+  res <- lapply(cli::cli_progress_along(threshold_steps), function(i) {
     out <- test_threshold(threshold_steps[i])
-    if (!is.null(out)) {
-      out$threshold <- threshold_steps[i]
-      res[[i]] <- out
-    }
-  }
+    if (!is.null(out)) out$threshold <- threshold_steps[i]
+    out
+  })
+
   res_df <- do.call(rbind.data.frame, res)
-  res_df$length <- res_df$cluster_end - res_df$cluster_start + 1
-  colnames(res_df)[colnames(res_df) %in% c("cluster_start", "cluster_end")] <- c("start", "end")
-  res_df[, c("threshold", "predictor", "cluster_id", "start", "end", "length", "statistic", "pvalue")]
+  res_df[, c("threshold", "predictor", "id", "start", "end", "length", "statistic", "pvalue")]
 }
