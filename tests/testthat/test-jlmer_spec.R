3863fb2b6432f1b3acf78eccc96632c707df403e
June Choe <yjchoe@sas.upenn.edu>
2023-05-17 17:18:09 -0400

    minimal test infra

---
+++ tests/testthat/test-jlmer_spec.R
@@ -0,0 +1,9 @@
+test_that("Minimal specification without grouping structures allowed", {
+  expect_equal(names(make_jlmer_spec(weight ~ 1 + Diet, ChickWeight)), c("formula", "data", "meta"))
+  expect_equal(names(make_jlmer_spec(weight ~ 1 + Diet + (1 | Chick), ChickWeight)), c("formula", "data", "meta"))
+})
+
+test_that("Inform misspecifications in grouping structure", {
+  expect_message(make_jlmer_spec(weight ~ 1 + Diet, subset(ChickWeight, Time <= 20), time = "Time"))
+  expect_message(make_jlmer_spec(weight ~ 1 + Diet, ChickWeight, subject = "Chick", time = "Time"))
+})
