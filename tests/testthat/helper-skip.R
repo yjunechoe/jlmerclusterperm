c3f2fc1fe2b13f3700a1f51bb0ad109228e9070f
June Choe <yjchoe@sas.upenn.edu>
2023-05-30 15:55:36 +0900

    fix skipping helper

--- tests/testthat/helper-skip.R
+++ tests/testthat/helper-skip.R
@@ -1,6 +1,7 @@
+no_julia <- !JuliaConnectoR::juliaSetupOk()
 skip_conditionally <- function() {
   skip_on_cran()
-  if (!JuliaConnectoR::juliaSetupOk()) {
+  if (no_julia) {
     skip("Julia setup not okay.")
   }
   invisible()
