44b3021dca7efd290c0b772303d513f3f16c62ca
June Choe <yjchoe@sas.upenn.edu>
2023-07-26 18:49:03 +0900

    usethis add release bullet to check julia 1.8

--- R/jlmerclusterperm-package.R
+++ R/jlmerclusterperm-package.R
@@ -5,3 +5,10 @@
 #' @importFrom stats setNames
 ## usethis namespace: end
 NULL
+
+### Usethis issue bullets
+release_bullets <- function() {
+  c(
+    "Repeat checks with Julia v1.8.0 (`juliaup default 1.8.0`)"
+  )
+}
