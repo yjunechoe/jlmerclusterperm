4e213ab7276316aba9ee9b674400525ae0ad5055
June Choe <yjchoe@sas.upenn.edu>
2023-10-04 13:13:24 -0400

    finish running rest of setup at top of tests

--- tests/testthat/test-aaa-a-logging.R
+++ tests/testthat/test-aaa-a-logging.R
@@ -2,7 +2,12 @@ skip_conditionally()
 
 stopJulia()
 system2("julia", '-e "using InteractiveUtils; println(versioninfo());"')
-start_with_threads(max_threads = 2, verbose = TRUE)
-set_projenv(verbose = TRUE)
+
+# Individually run `jlmerclusterperm_setup()`
+jlmerclusterperm:::start_with_threads(max_threads = 2, verbose = TRUE)
+jlmerclusterperm:::set_projenv(verbose = TRUE)
 cat(JuliaConnectoR::juliaCall("Pkg.status"))
 cat(readLines(file.path(jlmerclusterperm:::.jlmerclusterperm$opts$projdir, "Manifest.toml")), sep = "\n")
+jlmerclusterperm:::source_jl(verbose = TRUE)
+jlmerclusterperm:::define_globals()
+jlmerclusterperm:::cleanup_jl()
