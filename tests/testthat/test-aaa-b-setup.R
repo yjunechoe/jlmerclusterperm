2b6a548b2de1e454ed06ea726f9e64097bbc2ae1
June Choe <yjchoe@sas.upenn.edu>
2024-02-16 10:37:26 -0500

    test for manifest cache

--- tests/testthat/test-aaa-b-setup.R
+++ tests/testthat/test-aaa-b-setup.R
@@ -9,6 +9,7 @@ skip_conditionally()
 test_that("Setup with seed works (use 2 for testing)", {
   options("jlmerclusterperm.nthreads" = 2)
   expect_true(jlmerclusterperm_setup(cache_dir = tempdir(), restart = TRUE))
+  expect_true(file.exists(file.path(tempdir(), "julia", "Manifest.toml")))
   expect_equal(JuliaConnectoR::juliaEval("Threads.nthreads()"), 2)
 })
 
